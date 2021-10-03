3cf402a819bee0f8ae8455b573278cf4c335f5b2
krpvart <nskwin32cih@gmail.com>
2021-10-03 21:00:12 +0700

    Add ItemShopActivity.kt

--- app/src/main/java/com/krpvartstudio/shopit/presentation/ItemShopActivity.kt
+++ app/src/main/java/com/krpvartstudio/shopit/presentation/ItemShopActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.krpvartstudio.shopit.R
 
-class ItemShop : AppCompatActivity() {
+class ItemShopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_shop)
